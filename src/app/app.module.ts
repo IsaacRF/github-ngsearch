7b6c0dbff0da9b134d2a9265a4f578b5bc8ee45a
Isaac R.F <isaacrf239@gmail.com>
2020-06-06 18:16:56 +0200

    Adds basic html and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-search/user-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-search',
+  templateUrl: './user-search.component.html',
+  styleUrls: ['./user-search.component.scss']
+})
+export class UserSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
